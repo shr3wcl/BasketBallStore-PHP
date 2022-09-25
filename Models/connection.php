c9dee026c8adabe4d9a15212fd65c7be337cd20d
Bin-08-01 <minhtri192035@gmail.com>
2022-09-25 15:52:22 +0700

    Initial commit

---
+++ Models/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+class connection
+{
+    var $conn;
+
+    function __construct()
+    {
+        $servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $db_name = 'saleWebsite';
+
+        $this->conn = new mysqli($servername, $username, $password, $db_name);
+        $this->conn->set_charset('utf8');
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+}
