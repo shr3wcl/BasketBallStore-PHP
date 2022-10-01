a28d95362baaafe450d6661061da6d8889d76470
Minh Trí <89400593+Bin-08-01@users.noreply.github.com>
2022-10-01 23:02:59 +0700

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,37 +1,41 @@
-function preventDefault(e) {
-    e.preventDefault();
-}
-
-$(document).ready(function (){
-    $("#register-container input").on("keyup", function (){
-        $(this).parent().children("h6").children("span").html("");
-    });
-    $('#register-container input[name="gender"]').change(()=>{
-        $('#register-container input[name="gender"]').parent().parent().children("h6").children("span").html("");
+$(document).ready(()=>{
+    $("#login-container input").on("keyup", (event)=>{
+        $(event.currentTarget).parent().children("label").children("span").html("");
+        $(event.currentTarget).parent().parent().children(".msg-check-login").html("");
     })
 })
+
+$(document).ready(function () {
+    $("#register-container input").on("keyup", function () {
+        $(this).parent().children("h6").children("span").html("");
+    });
+    $('#register-container input[name="gender"]').change(() => {
+        $('#register-container input[name="gender"]').parent().parent().children("h6").children("span").html("");
+    });
+})
+
 
 
 $(document).ready(function () {
     $("#button-register").click(function (event) {
         event.preventDefault();
         $.post(
-            "Middlewares/login.php", {
-                firstname: $("#reg-firstname").val(),
-                lastname: $("#reg-lastname").val(),
-                gender: $('input[name="gender"]:checked').val(),
-                user: $("#reg-username").val(),
-                pass: $("#reg-password").val(),
-                repass: $("#reg-re-password").val(),
-                email: $("#reg-email").val(),
-                phone: $("#reg-phone").val()
-            },
+            "Middlewares/register.php", {
+            firstname: $("#reg-firstname").val(),
+            lastname: $("#reg-lastname").val(),
+            gender: $('input[name="gender"]:checked').val(),
+            user: $("#reg-username").val(),
+            pass: $("#reg-password").val(),
+            repass: $("#reg-re-password").val(),
+            email: $("#reg-email").val(),
+            phone: $("#reg-phone").val()
+        },
             function (data) {
                 const msg = JSON.parse(data);
                 let check = true;
-                for (const each in msg){
-                    if(msg[each]){
-                        check=false;
+                for (const each in msg) {
+                    if (msg[each]) {
+                        check = false;
                     }
                 }
                 if (!check) {
@@ -46,10 +50,40 @@ $(document).ready(function () {
                     $("#register-container .msg-check-phone").html(msg?.msgPhone);
                     console.log(msg);
                 }
-                else{
+                else {
                     window.location = "?page=login";
                 }
             }
         );
     });
 });
+
+$(document).ready(function () {
+    $("#button-login").click((e) => {
+        e.preventDefault();
+        $.post(
+            "Middlewares/login.php",
+            {
+                username: $("#input-username-login").val(),
+                password: $("#input-password-login").val()
+            },
+            (data) => {
+                const msg = JSON.parse(data);
+                console.log(msg);
+                let check = true;
+                for (const each in msg) {
+                    if (msg[each]) {
+                        check = false;
+                    }
+                }
+                if (!check) {
+                    $("#login-container .msg-check-username").html(msg?.msgUsername);
+                    $("#login-container .msg-check-password").html(msg?.msgPassword);
+                    $("#login-container .msg-check-login").html(msg?.msgLogin);
+                }else{
+                    window.location = "?page=home";
+                }
+            }
+        )
+    })
+})
