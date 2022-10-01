7e71f6255198ff580a47e2eab1ad0e8f9fbcd2a3
Minh Trí <89400593+Bin-08-01@users.noreply.github.com>
2022-10-01 22:59:58 +0700

    Update login.php

--- Middlewares/login.php
+++ Middlewares/login.php
@@ -1,15 +1,10 @@
 <?php
 require_once __DIR__ . '/../Controllers/LoginController.php';
 $LoginController = new LoginController();
-$fn = $_POST['firstname'];
-$ln = $_POST['lastname'];
-$g = $_POST['gender'] ?? "";
-$us = $_POST['user'];
-$pw = $_POST['pass'];
-$tpw = $_POST['repass'];
-$e = $_POST['email'];
-$p = $_POST['phone'];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
 try {
-    echo json_encode($LoginController->handleRegexRegister($fn, $ln, $g, $us, $pw, $tpw, $e, $p), JSON_THROW_ON_ERROR);
+    echo json_encode($LoginController->handleLogin($username, $password), JSON_THROW_ON_ERROR);
 } catch (JsonException $e) {
 }
