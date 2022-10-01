cfc1095a841d831e7b641d8c9c199ea2c582b5fd
Bin-08-01 <minhtri192035@gmail.com>
2022-10-01 23:09:41 +0700

    🚀Hotfix

--- Models/check.php
+++ Models/check.php
@@ -10,7 +10,8 @@ class check extends model{
         $this->conn = $conn_object->conn;
     }
 
-    function checkEmpty($value){
+    public function checkEmpty($value): string
+    {
         if(empty($value) && $value !== '0'){
             return "Require";
         }
@@ -18,7 +19,8 @@ class check extends model{
     }
 
 
-    function checkUsernameReg($un){
+    public function checkUsernameReg($un): string
+    {
         if(strlen($un)<6){
             return "Must have at least 6 characters";
         }
@@ -29,7 +31,8 @@ class check extends model{
         return "";
     }
 
-    function checkPassword($pw, $tpw){
+    public function checkPassword($pw, $tpw): string
+    {
         if(strlen($pw)<6){
             return "Must have at least 6 characters";
         }
@@ -39,7 +42,8 @@ class check extends model{
         return "";
     }
 
-    function checkEmailReg($e){
+    public function checkEmailReg($e): string
+    {
         $sql = "SELECT * FROM user where email = '{$e}'";
         $regExEmail = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
         $checkRegEx = preg_match($regExEmail, $e);
@@ -52,7 +56,8 @@ class check extends model{
         return "";
     }
 
-    function checkPhoneReg($p){
+    public function checkPhoneReg($p): string
+    {
         $sql = "SELECT * FROM user where phone = '{$p}'";
         $regExPhone = '/^0[0-9]{8}$/';
         $checkRegEx = preg_match($regExPhone, $p);
