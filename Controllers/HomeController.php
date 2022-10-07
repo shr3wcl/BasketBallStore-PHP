5c56418f02ffc20489686cdd4c19b9824e7a2e4e
Bin-08-01 <minhtri192035@gmail.com>
2022-10-07 22:35:39 +0700

    🚀Update image for home page

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,18 +1,20 @@
 <?php
 require_once __DIR__ . "/../Models/home.php";
-class HomeController{
+class HomeController
+{
     public home $home_model;
-    public function __construct(){
+    public function __construct()
+    {
         $this->home_model = new home();
     }
 
     public function list(): void
     {
-        $data_banner = $this->home_model->getBanner(1,3);
+        $data_banner = $this->home_model->getBanner(1, 3);
         $data_typical_products = $this->home_model->getTypicalProducts(0, 3);
         $data_newest_product = $this->home_model->getLatestProducts(0, 8);
-        $data_outstanding_product = $this->home_model->getOutstandingProduct(0, 13);
-        require_once ("Views/index.php");
+        $data_outstanding_product = $this->home_model->getOutstandingProduct(0, 15, 1);
+        $data_out_standing_pants = $this->home_model->getOutstandingProduct(0, 7, 2);
+        require_once("Views/index.php");
     }
-
 }
