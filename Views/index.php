48969803374da9aa5106249042d67c26f43aef02
Bin-08-01 <minhtri192035@gmail.com>
2022-11-18 18:59:24 +0700

    🚀Fix UI

--- Views/index.php
+++ Views/index.php
@@ -45,11 +45,5 @@
 <script src="public/js/main.js" type="text/javascript"></script>
 <script src="public/js/va-su.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js" integrity="sha512-zlWWyZq71UMApAjih4WkaRpikgY9Bz1oXIW5G0fED4vk14JjGlQ1UmkGM392jEULP8jbNMiwLWdM8Z87Hu88Fw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<style>
-
-</style>
-<script>
-
-</script>
 </body>
 </html>
